19ba20efc698f79e97dd36a19b9dc48ffaa0ea56
Ireneous <65375698+ireneous@users.noreply.github.com>
2025-07-31 17:29:15 +0000

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -13,14 +13,14 @@ if name and weight and height:
     st.subheader(f"{name}, your BMI is {round(bmi, 2)}")
 
     if bmi < 18.5:
-        st.write("You are underweight.")
+        st.write("You are underweight. You need to start eating more.")
     elif bmi < 25:
-        st.write("You have a normal weight.")
+        st.write("You have a normal weight. Keep it up!")
     elif bmi < 30:
-        st.write("You are overweight.")
+        st.write("You are overweight. Time to start running!")
     elif bmi < 35:
-        st.write("You are obese.")
+        st.write("You are obese. Please take action.")
     elif bmi < 40:
-        st.write("You are severely obese.")
+        st.write("You are severely obese. Health at risk.")
     else:
-        st.write("You are morbidly obese.")
+        st.write("You are morbidly obese. Take your life seriously!")
