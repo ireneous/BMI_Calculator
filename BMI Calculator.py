6fb62e4a3d0cf1eea442e1c627c16335384e0ef7
Ireneous <65375698+ireneous@users.noreply.github.com>
2025-07-31 00:31:22 +0000

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,101 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-#BMI Calculator
-
-
-# In[20]:
-
+# BMI Calculator in Python
 
 name = input("Enter your name: ")
-
 weight = int(input("Enter your weight in pounds: "))
-
 height = int(input("Enter your height in inches: "))
 
 BMI = (weight * 703) / (height * height)
 
-print(BMI)
+print("Your BMI is:", round(BMI, 2))
 
 if BMI > 0:
-    if(BMI < 18.5):
-        print(name + ", You are under weight.You need to start eating much lol")
-    elif (BMI <= 24.9):
-        print(name + ", You are normal weight. Keep working out")
-    elif (BMI < 29.9):
-        print(name + ", You are overweight. You need to start running to burn some fats")
-    elif (BMI < 34.9):
-        print(name + ", You are obese. You will explore soon lmao")
-    elif (BMI < 39.9):
-        print(name + ", You are Severely obese. hmmmm, too bad!!")
+    if BMI < 18.5:
+        print(name + ", you are underweight. You need to start eating more.")
+    elif BMI <= 24.9:
+        print(name + ", you are normal weight. Keep it up!")
+    elif BMI <= 29.9:
+        print(name + ", you are overweight. Time to start running!")
+    elif BMI <= 34.9:
+        print(name + ", you are obese. Please take action.")
+    elif BMI <= 39.9:
+        print(name + ", you are severely obese. Health at risk.")
     else:
-        print(name + ", Morbidly Obese. Take your life serious!!!")
+        print(name + ", you are morbidly obese. Take your life seriously!")
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-Under 18.5	Underweight	Minimal
-18.5 - 24.9	Normal Weight	Minimal
-25 - 29.9	Overweight	Increased
-30 - 34.9	Obese	High
-35 - 39.9	Severely Obese	Very High
-40 and over	Morbidly Obese	Extremely High
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# BMI Reference Chart:
+# ---------------------
+# Under 18.5        - Underweight         - Minimal Risk
+# 18.5 - 24.9       - Normal Weight       - Minimal Risk
+# 25 - 29.9         - Overweight          - Increased Risk
+# 30 - 34.9         - Obese               - High Risk
+# 35 - 39.9         - Severely Obese      - Very High Risk
+# 40 and over       - Morbidly Obese      - Extremely High Risk
